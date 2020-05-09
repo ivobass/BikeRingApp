b7e51699974d8eccc50bce726c7aeac918625541
Ivo Baptista <ivobass@gmail.com>
2020-05-09 21:09:41 +0100

    actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,6 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(BikeRingApp());
 
 class BikeRingApp extends StatelessWidget {
-
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,76 +27,77 @@ class _BikeRingHomeState extends State<BikeRingHome> {
     final player = AudioCache();
     player.play('ringbike.wav');
   }
-  
 
   double _fat = 300.0;
   @override
   Widget build(BuildContext context) {
     return Container(
-       child:SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Center(
-                child: AnimatedContainer(
-                  duration: Duration(seconds: 2),
-                  child: FlatButton(
-                    child: Container(
-                      child: Image.asset(
-                        'assets/screwmini.png',
-                        height: 20,
-                        width: 20,
-                      ),
-                      width: _fat,
-                      height: _fat,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.bottomCenter,
-                          colors: const [
-                            Colors.white70,
-                            Colors.white60,
-                            Colors.white70,
-                            Colors.white60,
-                            Colors.white60,
-                            Colors.white60,
-                            Colors.white38,
-                            Colors.white,
-                            Colors.white60,
-                            Colors.grey,
-                            Colors.white54,
-                            Colors.white70,
-                            Colors.white60,
-                            Colors.white70,
-                            Colors.white60,
-                            Colors.white60,
-                            Colors.white60,
-                            Colors.white38,
-                          ],
-                        ),
-                        color: Colors.yellow,
-                        shape: BoxShape.circle,
-                        boxShadow: const [
-                          BoxShadow(blurRadius: 30),
-                        ],
-                      ),
+      child: SafeArea(
+        child: Stack(
+          fit: StackFit.loose,
+          children: <Widget>[
+            AnimatedContainer(
+              duration: Duration(seconds: 1),
+              child: FlatButton(
+                child: Center(
+                  child: Container(
+                    child: Image.asset(
+                      'assets/screwmicro.png',
+                      width: 20.0,
+                      height: 20.0,
                     ),
-                    onPressed: () {
-                 setState(() {
-                        _fat = _fat + 2;
-                      print('$_fat');
-                      if (_fat == 306.0) {
-                        _fat = 300.0;
-                      }
-                      playSound();
-                 });
-                    },
+                    width: _fat,
+                    height: _fat,
+                    decoration: BoxDecoration(
+                      gradient: SweepGradient(
+                        
+                          colors: [
+                            Colors.grey.shade600,
+                            Colors.grey.shade300,
+                            Colors.grey.shade600,
+                            Colors.grey.shade700,
+                            Colors.grey.shade600,
+                            Colors.grey.shade400,
+                            Colors.grey.shade200,
+                            Colors.grey.shade500,
+                            Colors.grey.shade500,
+                            Colors.grey.shade800,
+                            Colors.grey.shade600,
+                            Colors.white,
+                              Colors.grey.shade200,
+                             Colors.grey.shade500,
+                            Colors.grey.shade500,
+                            Colors.grey.shade300,
+                            Colors.grey.shade400,
+                            Colors.grey.shade600,
+                            Colors.grey.shade600,
+                            Colors.grey.shade700,
+                            Colors.grey.shade800,
+                            Colors.grey.shade600,
+                          ]),
+                      color: Colors.yellow,
+                      shape: BoxShape.circle,
+                      boxShadow: const [
+                        BoxShadow(blurRadius: 30),
+                      ],
+                    ),
                   ),
                 ),
+                onPressed: () {
+                  setState(() {
+                    _fat = _fat + 2;
+                    print('$_fat');
+                    if (_fat == 306.0) {
+                      _fat = 300.0;
+                    }
+                    playSound();
+                  });
+                },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
